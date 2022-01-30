0087c64a6f70c7698baf0647f6e65113010e6df8
koheitakahashi <kouhei.takahashi@everyleaf.com>
2022-01-30 12:10:24 +0900

    GoogleMapsAPIを使ってみた

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_014430) do
+ActiveRecord::Schema.define(version: 2022_01_29_124702) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "maps", force: :cascade do |t|
+    t.text "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
